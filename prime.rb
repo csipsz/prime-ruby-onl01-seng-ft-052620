8be563d07419452fff0878754a2af3cc8ddc55de
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-27 23:09:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,10 @@ require 'pry'
 
 def prime?(num)
   divisors = (2..num - 1).to_a
-  divisors.none? {|divisor| num % divisor == 0} 
-  false
+  if divisors.none? {|divisor| num % divisor == 0} 
+    true
+  else
+    false 
+  end
+  
 end
