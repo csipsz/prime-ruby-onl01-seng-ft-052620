d4a8ad588322b10cefcae689b2cb936f28e90a38
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-27 22:49:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,6 @@ require 'pry'
 
 def prime?(num)
   dividers = (2..(num - 1)).to_a
-  dividers.any? {|divider| binding.pry num % divider == 0}
+  
+  dividers.any? {|divider| num % divider == 0}
 end
