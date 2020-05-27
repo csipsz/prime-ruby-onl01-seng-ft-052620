74c9f7cebf4aac19e88268dae45fe07fdaf5b667
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-27 23:06:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,5 @@
 require 'pry'
 
 def prime?(num)
-  if num > 2 
-    if num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0
-      false 
-    end
-  end 
-  true
+  divisors = (2..num - 1).to_a
 end
